9949b51f32693bd486551aa59bb7c1244d5ae9ff
ChamodKalupahana <chamodkalupahana@gmail.com>
2024-08-21 18:25:42 +0100

    Test commit

---
+++ SwiftfulThinkingBootcamp/SwiftfulThinkingBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingBootcampApp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Chamod on 21/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
